89cb3fb50daff26eb3398eb011a747eaa9b6935e
José Pereira <jose.a.pereira03@gmail.com>
2023-05-18 12:09:23 +0100

    Organize code

--- bimaru2.py
+++ bimaru2.py
@@ -43,34 +43,29 @@ class Board:
         self.limit_columns = columns
         self.current_rows = [0] * 10
         self.current_columns = [0] * 10
-        
+
         # create matrix for the board cells
         self.cells = np.matrix([[' ' for x in range(len(rows))]
                                for y in range(len(columns))])
-        
 
     def get_value(self, row: int, col: int) -> str:
         """Devolve o valor na respetiva posição do tabuleiro."""
         if (self.valid_cell(row, col)):
             return self.cells[row, col]
-        else:
-            return None
 
     def valid_cell(self, row: int, col: int) -> bool:
         """Verifica se a célula é válida."""
         return not (row < 0 or row > const.BOARD_SIZE or col < 0 or col > const.BOARD_SIZE)
 
-    def set_value(self, row: int, col: int, value: str):
+    def set_value(self, row: int, col: int, value: str) -> None:
         """Atribui o valor na respetiva posição do tabuleiro."""
         if (self.valid_cell(row, col) and self.cells[row, col] == const.EMPTY):
             self.cells[row, col] = value
-            
-            if(value not in ['W', '.']):
+
+            if (value not in ['W', '.']):
                 self.current_rows[row] += 1
                 self.current_columns[col] += 1
-        else:
-            return
-    
+
     def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
         """Devolve os valores imediatamente acima e abaixo,
         respectivamente."""
@@ -80,32 +75,32 @@ class Board:
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
         return (self.get_value(row, col-1), self.get_value(row, col+1))
-    
+
     def is_adjacent_water_horizontal(self, row: int, col: int) -> bool:
         """Verifica se a célula está adjacente a água."""
         up, down = self.adjacent_vertical_values(row, col)
-        return (up in ['.', 'W'] or down in ['.', 'W'] )
-    
+        return (up in ['.', 'W'] or down in ['.', 'W'])
+
     def is_adjacent_water_vertical(self, row: int, col: int) -> bool:
         """Verifica se a célula está adjacente a água."""
         left, right = self.adjacent_horizontal_values(row, col)
-        return (left in ['.', 'W'] or right in ['.', 'W'] )
-    
-    def fill_row_with_water(self, row: int):
+        return (left in ['.', 'W'] or right in ['.', 'W'])
+
+    def fill_row_with_water(self, row: int) -> None:
         """Preenche a linha 'row' com água."""
         for col in range(const.BOARD_SIZE+1):
-            if(self.get_value(row, col) == const.EMPTY):
-                self.set_value(row, col, '.')
-    
-    def fill_column_with_water(self, col: int): 
-        """Preenche a coluna 'col' com água."""
-        for row in range(const.BOARD_SIZE+1):
-            if(self.get_value(row, col) == const.EMPTY):
+            if (self.get_value(row, col) == const.EMPTY):
                 self.set_value(row, col, '.')
 
-    def fill_segments_with_water(self, row: int, col: int, length: int, orientation: str):
-        """Preenche os segmentos de água adjacentes à posição (row, col)."""
-        
+    def fill_column_with_water(self, col: int) -> None:
+        """Preenche a coluna 'col' com água."""
+        for row in range(const.BOARD_SIZE+1):
+            if (self.get_value(row, col) == const.EMPTY):
+                self.set_value(row, col, '.')
+
+    def fill_segments_with_water(self, row: int, col: int, length: int, orientation: str) -> None:
+        """Preenche segmentos de água"""
+
         if (orientation == const.HORIZONTAL):
             for i in range(length):
                 cell = self.get_value(row, col+i)
@@ -116,17 +111,86 @@ class Board:
                 cell = self.get_value(row+i, col)
                 if (cell == const.EMPTY):
                     self.set_value(row+i, col, '.')
-    
-    def fill_exausted_rows_cols(self):
+
+    def fill_exausted_rows_cols(self) -> None:
         """Preenche as linhas e colunas que já estão completas."""
         for i in range(const.BOARD_SIZE+1):
-            if(self.current_rows[i] == self.limit_rows[i]):
+            if (self.current_rows[i] == self.limit_rows[i]):
                 self.fill_row_with_water(i)
             if (self.current_columns[i] == self.limit_columns[i]):
                 self.fill_column_with_water(i)
 
+    def surround_circle(self, row: int, col: int) -> None:
+        for i in range(3):
+            self.fill_segments_with_water(
+                row - 1 + i, col - 1, 3, const.HORIZONTAL)
+
+    def surround_top(self, row: int, col: int) -> None:
+        for i in range(3):
+            if (i == 1):
+                self.fill_segments_with_water(
+                    row - 1, col, 1, const.VERTICAL)
+            else:
+                self.fill_segments_with_water(
+                    row - 1, col - 1 + i, 4, const.VERTICAL)
+
+    def surround_bottom(self, row: int, col: int) -> None:
+        for i in range(3):
+            if (i == 1):
+                self.fill_segments_with_water(
+                    row + 1, col, 1, const.VERTICAL)
+            else:
+                self.fill_segments_with_water(
+                    row - 2, col - 1 + i, 4, const.VERTICAL)
+
+    def surround_left(self, row: int, col: int) -> None:
+        for i in range(3):
+            if (i == 1):
+                self.fill_segments_with_water(
+                    row, col - 1, 1, const.HORIZONTAL)
+            else:
+                self.fill_segments_with_water(
+                    row - 1 + i, col - 1, 4, const.HORIZONTAL)
+
+    def surround_right(self, row: int, col: int) -> None:
+        for i in range(3):
+            if (i == 1):
+                self.fill_segments_with_water(
+                    row, col + 1, 1, const.HORIZONTAL)
+            else:
+                self.fill_segments_with_water(
+                    row - 1 + i, col - 2, 4, const.HORIZONTAL)
+
+    def surround_middle(self, row: int, col: int) -> None:
+        if (self.is_adjacent_water_vertical(row, col)):
+            self.fill_segments_with_water(
+                row - 2, col - 1, 5, const.VERTICAL)
+            self.fill_segments_with_water(
+                row - 2, col + 1, 5, const.VERTICAL)
+        elif (self.is_adjacent_water_horizontal(row, col)):
+            self.fill_segments_with_water(
+                row - 1, col - 2, 5, const.HORIZONTAL)
+            self.fill_segments_with_water(
+                row + 1, col - 2, 5, const.HORIZONTAL)
+
+    def surround_hint(self, row: int, col: int, value: str) -> None:
+        match value:
+            case 'C':
+                self.surround_circle(row, col)
+            case 'T':
+                self.surround_top(row, col)
+            case 'B':
+                self.surround_bottom(row, col)
+            case 'R':
+                self.surround_right(row, col)
+            case 'L':
+                self.surround_left(row, col)
+            case 'M':
+                self.surround_middle(row, col)
+        
+
     @staticmethod
-    def parse_instance():
+    def parse_instance() -> None:
         """Lê o test do standard input (stdin) que é passado como argumento
         e retorna uma instância da classe Board.
 
@@ -139,86 +203,38 @@ class Board:
         rows = []
         columns = []
         hints = []
+
         while True:
             instance = stdin.readline().split()
             line = [int(x) if x.isdigit() else x for x in instance]
             if not line:
                 break
-
             if (line[0] == const.ROW):
                 rows = line[1:]
-
             elif (line[0] == const.COLUMN):
                 columns = line[1:]
-
             elif (line[0] == const.HINT):
                 hints.append(line[1:])
-
             else:
                 continue
-            
-        board = Board(rows, columns)
-        
+
+        new_board = Board(rows, columns)
+
         # add hints
         for hint in hints:
             row, col, value = hint[0], hint[1], hint[2]
-            board.set_value(row, col, value)
-        
-        # fill obvious rows/cols with water   
-        board.fill_exausted_rows_cols()
-        
+            new_board.set_value(row, col, value)
+
+        # fill obvious rows/cols with water
+        new_board.fill_exausted_rows_cols()
+
         # surround hints with water
         for hint in hints:
             row, col, value = hint[0], hint[1], hint[2]
-            # circle hint
-            if (value == 'C'):
-                for i in range(3):
-                    board.fill_segments_with_water(row - 1 + i, col - 1, 3, const.HORIZONTAL)
-            # top hint
-            if (value == 'T'):
-                for i in range(3):
-                    if (i == 1):
-                        board.fill_segments_with_water(row - 1, col, 1, const.VERTICAL)
-                    else:
-                        board.fill_segments_with_water(row - 1, col - 1 + i, 4, const.VERTICAL)
 
-            # bottom hint
-            if  (value == 'B'):
-                for i in range(3):
-                    if (i == 1):
-                        board.fill_segments_with_water(row + 1, col, 1, const.VERTICAL)
-                    else:
-                        board.fill_segments_with_water(row - 2, col - 1 + i, 4, const.VERTICAL)
+            new_board.surround_hint(row, col, value)
 
-            # left hint
-            if (value == 'R'):
-                for i in range(3):
-                    if (i == 1):
-                        board.fill_segments_with_water(row, col + 1, 1, const.HORIZONTAL)
-                    else:
-                        board.fill_segments_with_water(row - 1 + i, col - 2, 4, const.HORIZONTAL)
-
-            # right hint
-            if (value == 'L'):
-                for i in range(3):
-                    if (i == 1):
-                        board.fill_segments_with_water(row, col - 1, 1, const.HORIZONTAL)
-                    else:
-                        board.fill_segments_with_water(row - 1 + i, col -1, 4, const.HORIZONTAL)
-
-
-                
-            # middle hint
-            if (value == 'M'):
-                if (board.is_adjacent_water_vertical(row, col) ):
-                    board.fill_segments_with_water(row - 2, col - 1, 5, const.VERTICAL)
-                    board.fill_segments_with_water(row - 2, col + 1, 5, const.VERTICAL)
-                elif (board.is_adjacent_water_horizontal(row, col)):
-                    board.fill_segments_with_water(row - 1, col - 2, 5, const.HORIZONTAL)
-                    board.fill_segments_with_water(row + 1, col - 2, 5, const.HORIZONTAL)
-     
-        return board
-    
+        return new_board
 
     def __str__(self):
         """Retorna uma string que representa o tabuleiro."""
@@ -226,7 +242,6 @@ class Board:
             return str(self.cells)
         else:
             return str(const.parse_to_debug(self.cells))
-
 
 
 class Bimaru(Problem):
@@ -264,7 +279,6 @@ class Bimaru(Problem):
     # TODO: outros metodos da classe
 
 
-
 if __name__ == "__main__":
     # TODO:
     # Ler o ficheiro do standard input,
@@ -273,11 +287,11 @@ if __name__ == "__main__":
     # Imprimir para o standard output no formato indicado.
 
     board = Board.parse_instance()
-    
+
     # Criar uma instância de Bimaru:
     problem = Bimaru(board)
     # Criar um estado com a configuração inicial:
-    
+
     initial_state = BimaruState(board)
 
     print(initial_state.board)
